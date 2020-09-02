27ee8290d332b77f5e0e38acd97ac29b250288fb
oluwadara <daraoloye99@gmail.com>
2020-09-02 14:02:23 +0100

    :hammer: Built the scripting for the refresh button

--- pond/static/pond/js/each_pond.js
+++ pond/static/pond/js/each_pond.js
@@ -1,22 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 	
-	document.getElementById("refresh").onclick = () => {
-
+	// document.getElementById("refresh").onclick = () => {
+	async function refresh() {
 		// get URL and hence channel ID
 		const api_url = new URL (window.location.href);
 		const n = api_url.pathname.length;
 		const channel_id = api_url.pathname.substring(1,n);
 
-		const Http = new XMLHttpRequest();
+		// const Http = new XMLHttpRequest();
 		
-		const url='https://api.thingspeak.com/channels/' + channel_id +'/feeds.json?results=1';
-		Http.open("GET", url);
-		Http.send();
+		const url='https://api.thingspeak.com/channels/' + channel_id +'/feeds.json?api_key=Z7POUDWMQBPRKQU1&results=1';
+		// console.log(url);
+		// Http.open("GET", url, false);
+		// Http.send();
 
-		Http.onreadystatechange = (e) => {
-			res = JSON.parse(Http.responseText);
-			document.getElementById("temp").value = res.feeds[0].field1;
-			document.getElementById("ph").value = res.feeds[0].field2;
-		}
-	}
-});
+		let response = await fetch(url)
+		let data = JSON.parse(await response.text())
+
+		// Http.onreadystatechange = (e) => {
+		// res = JSON.parse(Http.responseText);
+		document.getElementById("temp").value = data.feeds[0].field1;
+		document.getElementById("ph").value = data.feeds[0].field2;
+		document.getElementById("water_level").value = data.feeds[0].field4;
+		// console.log(data)
+		// }
+	}	
+// }
+		
+// });
